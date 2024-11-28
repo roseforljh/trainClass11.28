da3001fbd284c2e5b64acf542d3f8f3bb1288615
SeeingFower Rose <3303@qq.com>
2024-11-28 12:33:21 +0800

    11.28  2024

--- src/views/index.vue
+++ src/views/index.vue
@@ -30,7 +30,7 @@
     <van-tabbar v-model="active" placeholder route>
       <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="apps-o" to="/classxx">分类</van-tabbar-item>
-      <van-tabbar-item badge="1" to="/shopCar" icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item badge="1" to="/shopCar" icon="shopping-cart-o">购物车111</van-tabbar-item>
       <van-tabbar-item icon="user-circle-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
